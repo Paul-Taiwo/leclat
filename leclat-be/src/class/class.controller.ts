753797e3ce3ae63a9e4170f5abb422f01d7caa72
Paul Taiwo <ayopaulot@gmail.com>
2022-06-19 13:06:04 +0100

    limit getClasses route to only admin

--- leclat-be/src/class/class.controller.ts
+++ leclat-be/src/class/class.controller.ts
@@ -1,6 +1,10 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 
 import { JwtGuard } from 'src/auth/guard';
+import { RolesGuard } from 'src/auth/guard/role.guard';
+import { Roles } from 'src/decorators/role.decorator';
+import { Role } from 'src/enum';
+
 import { ClassService } from './class.service';
 
 @UseGuards(JwtGuard)
@@ -9,7 +13,9 @@ export class ClassController {
   constructor(private readonly classService: ClassService) {}
 
   @Get()
-  getHello(): any {
+  @Roles(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  getClasses(): any {
     return this.classService.getClasses();
   }
 }
