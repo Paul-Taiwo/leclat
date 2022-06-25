87bb72e75f12002d3716f3529a44264dcbbe5abb
Paul Taiwo <ayopaulot@gmail.com>
2022-06-26 00:58:06 +0100

    Update course.dto.ts

--- leclat-be/src/dto/course.dto.ts
+++ leclat-be/src/dto/course.dto.ts
@@ -1,17 +1,13 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CourseDto {
-  @IsEmail()
-  @IsNotEmpty()
-  email: string;
-
   @IsString()
   @IsNotEmpty()
   name: string;
 
   @IsString()
   @IsNotEmpty()
-  phone: string;
+  code: string;
 
   @IsString()
   @IsNotEmpty()
