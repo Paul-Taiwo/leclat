c389cb04e1b7a870d47763917625a91cb047b6e5
Paul Taiwo <ayopaulot@gmail.com>
2022-06-26 01:03:11 +0100

    feat(): get all course

--- leclat-be/src/course/course.controller.ts
+++ leclat-be/src/course/course.controller.ts
@@ -15,41 +15,42 @@ import { RolesGuard } from 'src/auth/guard/role.guard';
 import { Roles } from 'src/decorators/role.decorator';
 import { CourseDto } from 'src/dto/course.dto';
 import { Role } from 'src/enum';
+
 import { CourseService } from './course.service';
 
 @UseGuards(JwtGuard)
-@Controller('users')
+@Controller('courses')
 export class CourseController {
-  constructor(private readonly userService: CourseService) {}
+  constructor(private readonly courseService: CourseService) {}
 
   @Get()
-  @Roles(Role.ADMIN)
+  @Roles(Role.USER)
   @UseGuards(RolesGuard)
   getCourses() {
-    return this.userService.getCourses();
+    return this.courseService.getCourses();
   }
 
-  @Post()
-  @Roles(Role.ADMIN)
-  @UseGuards(RolesGuard)
-  addCourse(@Body() dto: CourseDto) {
-    return this.userService.addCourse(dto);
-  }
+  // @Post()
+  // @Roles(Role.ADMIN)
+  // @UseGuards(RolesGuard)
+  // addCourse(@Body() dto: CourseDto) {
+  //   return this.courseService.addCourse(dto);
+  // }
 
-  @Patch(':id')
-  @Roles(Role.ADMIN)
-  @UseGuards(RolesGuard)
-  editCourse(
-    @Param('id', ParseUUIDPipe) userId: string,
-    @Body() dto: CourseDto,
-  ) {
-    return this.userService.editCourse(userId, dto);
-  }
+  // @Patch(':id')
+  // @Roles(Role.ADMIN)
+  // @UseGuards(RolesGuard)
+  // editCourse(
+  //   @Param('id', ParseUUIDPipe) courseId: string,
+  //   @Body() dto: CourseDto,
+  // ) {
+  //   return this.courseService.editCourse(courseId, dto);
+  // }
 
-  @Delete(':id')
-  @Roles(Role.ADMIN)
-  @UseGuards(RolesGuard)
-  deleteCourse(@Param('id', ParseUUIDPipe) userId: string) {
-    return this.userService.deleteCourse(userId);
-  }
+  // @Delete(':id')
+  // @Roles(Role.ADMIN)
+  // @UseGuards(RolesGuard)
+  // deleteCourse(@Param('id', ParseUUIDPipe) courseId: string) {
+  //   return this.courseService.deleteCourse(courseId);
+  // }
 }
--- leclat-be/src/course/course.service.ts
+++ leclat-be/src/course/course.service.ts
@@ -14,138 +14,135 @@ export class CourseService {
   constructor(private prisma: PrismaService) {}
 
   async getCourses() {
-    const users = await this.prisma.user.findMany({
+    const courses = await this.prisma.course.findMany({
       include: {
-        class: true,
-      },
-    });
-    const filteredCourse = users.map((user) => {
-      const userObj = { ...user };
-      delete userObj.password;
-      return userObj;
-    });
-
-    return {
-      users: filteredCourse,
-    };
-  }
-
-  async addCourse({ email, name, phone, classId }: CourseDto) {
-    /* Hashing the password. */
-    const password = await hash('password');
-
-    try {
-      /* Checking if the class has a user. */
-      const { Course } = await this.prisma.class.findUnique({
-        where: {
-          id: classId,
-        },
-        select: {
-          Course: true,
-        },
-      });
-
-      /* Checking if the class has a user. */
-      if (Course && Course.length) {
-        throw new BadRequestException('Class can only have one user');
-      }
-
-      /* Checking if the user with the email already exists. */
-      const userInDb = await this.prisma.user.findUnique({
-        where: {
-          email,
-        },
-      });
-
-      /* Checking if the user with the email already exists. */
-      if (userInDb) {
-        throw new BadRequestException(
-          `Course with email: ${email} is already attached to a class`,
-        );
-      }
-
-      /* Creating a user. */
-      const user = await this.prisma.user.create({
-        data: {
-          email,
-          name,
-          phone,
-          classId,
-          password,
-        },
-      });
-
-      /* Deleting the password from the user object before returning it. */
-      delete user.password;
-
-      return {
-        message: 'Course Created',
-        data: user,
-      };
-    } catch (error) {
-      if (error instanceof PrismaClientKnownRequestError) {
-        if (error.code === 'P2002') {
-          throw new ForbiddenException('Credentials taken');
-        }
-      }
-      throw error;
-    }
-  }
-
-  async editCourse(userId: string, dto: CourseDto) {
-    // get the user by id
-    const user = await this.prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
-    });
-
-    /* Checking if the user exists. */
-    if (user === null) {
-      throw new BadRequestException(`No user with id:${userId} found`);
-    }
-
-    /* Updating the user with the new data. */
-    const updatedCourse = await this.prisma.user.update({
-      where: {
-        id: userId,
-      },
-      data: {
-        ...dto,
-      },
-    });
-
-    /* Deleting the password from the user object before returning it. */
-    delete updatedCourse.password;
-
-    return {
-      message: 'Course updated successfully',
-      user: { ...updatedCourse },
-    };
-  }
-
-  async deleteCourse(userId: string) {
-    // get the user by id
-    const user = await this.prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
-    });
-
-    /* Checking if the user exists. */
-    if (user === null) {
-      throw new BadRequestException(`No user with id:${userId} found`);
-    }
-
-    /* Deleting the user. */
-    await this.prisma.user.delete({
-      where: {
-        id: userId,
+        Lecturer: true,
+        Period: true,
       },
     });
 
     return {
-      message: 'Course deleted successfully',
+      message: 'success',
+      data: courses,
     };
   }
+
+  // async addCourse({ email, name, phone, classId }: CourseDto) {
+  //   /* Hashing the password. */
+  //   const password = await hash('password');
+
+  //   try {
+  //     /* Checking if the class has a user. */
+  //     const { Course } = await this.prisma.class.findUnique({
+  //       where: {
+  //         id: classId,
+  //       },
+  //       select: {
+  //         Course: true,
+  //       },
+  //     });
+
+  //     /* Checking if the class has a user. */
+  //     if (Course && Course.length) {
+  //       throw new BadRequestException('Class can only have one user');
+  //     }
+
+  //     /* Checking if the user with the email already exists. */
+  //     const userInDb = await this.prisma.user.findUnique({
+  //       where: {
+  //         email,
+  //       },
+  //     });
+
+  //     /* Checking if the user with the email already exists. */
+  //     if (userInDb) {
+  //       throw new BadRequestException(
+  //         `Course with email: ${email} is already attached to a class`,
+  //       );
+  //     }
+
+  //     /* Creating a user. */
+  //     const user = await this.prisma.user.create({
+  //       data: {
+  //         email,
+  //         name,
+  //         phone,
+  //         classId,
+  //         password,
+  //       },
+  //     });
+
+  //     /* Deleting the password from the user object before returning it. */
+  //     delete user.password;
+
+  //     return {
+  //       message: 'Course Created',
+  //       data: user,
+  //     };
+  //   } catch (error) {
+  //     if (error instanceof PrismaClientKnownRequestError) {
+  //       if (error.code === 'P2002') {
+  //         throw new ForbiddenException('Credentials taken');
+  //       }
+  //     }
+  //     throw error;
+  //   }
+  // }
+
+  // async editCourse(userId: string, dto: CourseDto) {
+  //   // get the user by id
+  //   const user = await this.prisma.user.findUnique({
+  //     where: {
+  //       id: userId,
+  //     },
+  //   });
+
+  //   /* Checking if the user exists. */
+  //   if (user === null) {
+  //     throw new BadRequestException(`No user with id:${userId} found`);
+  //   }
+
+  //   /* Updating the user with the new data. */
+  //   const updatedCourse = await this.prisma.user.update({
+  //     where: {
+  //       id: userId,
+  //     },
+  //     data: {
+  //       ...dto,
+  //     },
+  //   });
+
+  //   /* Deleting the password from the user object before returning it. */
+  //   delete updatedCourse.password;
+
+  //   return {
+  //     message: 'Course updated successfully',
+  //     user: { ...updatedCourse },
+  //   };
+  // }
+
+  // async deleteCourse(userId: string) {
+  //   // get the user by id
+  //   const user = await this.prisma.user.findUnique({
+  //     where: {
+  //       id: userId,
+  //     },
+  //   });
+
+  //   /* Checking if the user exists. */
+  //   if (user === null) {
+  //     throw new BadRequestException(`No user with id:${userId} found`);
+  //   }
+
+  //   /* Deleting the user. */
+  //   await this.prisma.user.delete({
+  //     where: {
+  //       id: userId,
+  //     },
+  //   });
+
+  //   return {
+  //     message: 'Course deleted successfully',
+  //   };
+  // }
 }
